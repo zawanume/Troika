d08699433a0c794f50e4a1e68e798948d40df095
zawanume <ume.8321@gmail.com>
2023-05-05 10:41:04 +0900

    usa command

---
+++ src/Commands/usa.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021-2022 mtripg6666tdr
+ * 
+ * This file is part of mtripg6666tdr/Discord-SimpleMusicBot. 
+ * (npm package name: 'discord-music-bot' / repository url: <https://github.com/mtripg6666tdr/Discord-SimpleMusicBot> )
+ * 
+ * mtripg6666tdr/Discord-SimpleMusicBot is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * mtripg6666tdr/Discord-SimpleMusicBot is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with mtripg6666tdr/Discord-SimpleMusicBot. 
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import type { CommandArgs } from ".";
+import type { CommandMessage } from "../Component/commandResolver/CommandMessage";
+
+import { BaseCommand } from ".";
+import { i18n } from "i18next";
+
+export default class Usa extends BaseCommand {
+  constructor(){
+    super({
+      //name: "アメリカ",
+      alias: ["usa", "us","america","ソ連","アメリカ","アメリカ合衆国","合衆国"],
+      //description: "アメリカ合衆国の国歌を再生します。",
+      unlist: false,
+      category: "player",
+      //permissionDescription: "なし",
+      shouldDefer: false,
+      requiredPermissionsOr: [],
+    });
+  }
+
+  async run(message:CommandMessage, options:CommandArgs, t:i18n["t"]){
+    options.server.updateBoundChannel(message);
+    const server = options.server;
+    // VCに入れない
+    if(!(await options.server.joinVoiceChannel(message, {replyOnFail:true}, t))) return;
+    // 一時停止されてるね
+    if(options.rawArgs === "" && server.player.isPaused){
+      server.player.resume();
+    }
+
+    server.player.stop();
+    await server.playFromURL(message, "https://www.youtube.com/watch?v=30FOxaYyVg0", {}, t);
+    await server.queue.removeAt(1);
+  }
+}
